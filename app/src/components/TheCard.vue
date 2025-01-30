abd51febe016a56cb1743d23d2b687cf4bb78c46
Kaibolt <kyle.salowitz08@gmail.com>
2025-01-30 12:31:43 -0500

    asdasdasdasdasdasda

---
+++ app/src/components/TheCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ glassItems.name }}</h2>
+    <h3>{{ glassItems.price }}</h3>
+  </div>
+</template>
+
+<script setup>
+const glassItems = [
+  {
+    name: 'Placeholder Name',
+    price: 'Placeholder Price',
+    image: '@/public/Glass_Items/3ee25c432c7c4099aa04163907fc100b.webp',
+  },
+  {
+    name: 'Placeholder Name2',
+    price: 'Placeholder Price2',
+    image: '@/public/Glass_Items/image-asset.jpeg',
+  },
+]
+</script>
+
+<style scoped></style>
