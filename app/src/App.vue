69e9166576f72d3f0c337b546a6c29072980c738
Kaibolt <kyle.salowitz08@gmail.com>
2025-01-31 12:25:28 -0500

    fake news

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import glassItems from '@/components/TheCard.vue'
 </script>
 
 <template>
