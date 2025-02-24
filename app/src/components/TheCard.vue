fd410b5077218adbaf25404906870dce13d63d29
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2025-02-23 23:42:29 -0500

    Checkpoint because I'm losing it
    If I have to change the code one more time for the sake of efficiency, I'm keeping it inefficient. I had a nice 13 hour nap before this, and I have hot cocoa right next to me. I can stay up until 5, take a nap on the bus, and be fine in the morning. FUTURE ME, I KNOW YOU'LL FORGET THIS!
    Prop for function, pass on function, worry about function in different file and all is fine!
    Next Step: Make the working shop file and app file merge into just the shop file so the app file is neat and tidey. Then calculate all costs, remove buttom and voila, nice and easy
    Lord have mercy, how did I sleep until 9:37PM

--- app/src/components/TheCard.vue
+++ app/src/components/TheCard.vue
@@ -1,6 +1,10 @@
 <script setup>
 import { allItems } from '@/assets/allItems.js'
-console.log(allItems)
+import { defineProps } from 'vue'
+const cartBase = defineProps({
+  addToCart: Function,
+})
+const cartBuy = (item) => cartBase.addtoCart(item) //If this doesn't work, I'm going to be very sad.
 </script>
 <template>
   <div v-for="allItem in allItems" class="card bg-base-100 w-96 shadow-xl">
@@ -13,7 +17,7 @@ console.log(allItems)
     </figure>
     <br />
     <div class="card-actions justify-center">
-      <button class="btn btn-primary">Buy Now</button>
+      <button @click="cartBuy(allItem)" class="btn btn-primary">Buy Now</button>
     </div>
     <br />
   </div>
